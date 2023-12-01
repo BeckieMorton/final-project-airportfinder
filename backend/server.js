e8bea864437dcf5e86423540b49a8397b6d813ef
BeckieMorton <beckam11@gmail.com>
2023-12-01 17:23:09 +0100

    backend and frontend file structure and 2 endpoints working

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,89 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import listEndpoints from "express-list-endpoints";
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+//------Connect to database------//
+const mongoUrl =
+  process.env.MONGO_URL || "mongodb://127.0.0.1:27017/Airportfinder";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
+
+//-----Check connection is working----//
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+
+//-----Define mongoose model based on Airport database----//
+const Airport = mongoose.model("Airport", {
+  id: Number,
+  ident: String,
+  type: String,
+  name: String,
+  latitude_deg: Number,
+  longitude_deg: Number,
+  elevation_ft: Number,
+  continent: String,
+  iso_country: String,
+  iso_region: String,
+  municipality: String,
+  scheduled_service: String,
+  gps_code: String,
+  iata_code: String,
+  local_code: String,
+  home_link: String,
+  wikipedia_link: String,
+  keywords: String,
+});
+
+//-------Defines the port the app will run on-----//
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+//--------Add middlewares-------//
 app.use(cors());
 app.use(express.json());
 
+//------------END POINTS----------//
+
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send(listEndpoints(app));
 });
+
+//----End point for all airports
+app.get("/airports", async (req, res) => {
+  const airports = await Airport.find();
+  res.json(airports);
+});
+
+//---End point for all airports in a country
+app.get("/airports/:iso_country", async (req, res) => {
+  const airportCountry = req.params.iso_country;
+  const regex = new RegExp("^" + airportCountry + "$", "i"); // Case-insensitive regex to make sure if user types lowercase it will still match with the database
+  Airport.find({ iso_country: { $regex: regex } }).then((iso_country) => {
+    if (iso_country) {
+      res.json(iso_country);
+    } else {
+      res.status(404).json({ error: `Country not found` });
+    }
+  });
+});
+
+//----End point for SINGLE result (IATA code)----//
+
+// app.get("/airports/iata/:iata_code", async (req, res) => {
+//   const iataCode = req.params.iata_code.toLowerCase();
+//   let byIata = Airport.find(
+//     (item) => item.iata_code.toLowerCase() === iataCode
+//   );
+
+//   if (byIata) {
+//     res.json(byIata);
+//   } else {
+//     res.status(404).send("No Airport was found");
+//   }
+// });
 
 // Start the server
 app.listen(port, () => {
