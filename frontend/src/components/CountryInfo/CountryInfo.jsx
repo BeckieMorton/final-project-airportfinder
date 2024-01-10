e29f7be3d320e4977c5995e01e754bad8ffcd9e7
BeckieMorton <beckam11@gmail.com>
2024-01-10 17:13:37 +1100

    fixed population in country info and formatted it

--- frontend/src/components/CountryInfo/CountryInfo.jsx
+++ frontend/src/components/CountryInfo/CountryInfo.jsx
@@ -33,12 +33,15 @@ export const CountryInfo = () => {
     return <div>loading country data</div>;
   }
 
+  console.log(`country info in this here:`, countryInfo);
+
   //destructure countryInfo to render relevant information
   const officialName = countryInfo[0].name.official;
   const continent = countryInfo[0].continents;
   const capital = countryInfo[0].capital;
   const flag = countryInfo[0].flags.svg;
   const flagAlt = countryInfo[0].flags.alt;
+  const population = countryInfo[0].population;
 
   const language = countryInfo.map((info) => {
     const firstLanguageValue = Object.values(info.languages)[0]; //
@@ -50,6 +53,11 @@ export const CountryInfo = () => {
     return firstCurrencyValue.name;
   });
 
+  // Function to format population with commas
+  const formatPopulation = (population) => {
+    return population.toLocaleString();
+  };
+
   return (
     <>
       <div>
@@ -60,7 +68,7 @@ export const CountryInfo = () => {
 
         <p>Continent: {continent}</p>
         <p>Capital: {capital}</p>
-        <p>Population: {capital}</p>
+        <p>Population: {formatPopulation(population)}</p>
         <p>Language: {language}</p>
         <p>Currency: {currency}</p>
       </div>
